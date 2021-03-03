e5599b4475f61c3a2e07ee817de2fd35cc98f4a9
jkijki1249@gmail.com <wkdehd123!>
2021-03-03 17:05:52 +0900

    백그라운드 코드만 받아오고 / gps에서 현재위치 받아오기는 성공 (하지만 실시간 갱신은 수행해야함)

--- app/src/main/java/com/example/deedo/HOME_ETC/Home_activity.java
+++ app/src/main/java/com/example/deedo/HOME_ETC/Home_activity.java
@@ -1,8 +1,10 @@
 package com.example.deedo.HOME_ETC;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -10,6 +12,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
@@ -20,6 +29,7 @@ import com.example.deedo.Friend.Modify_Friend;
 import com.example.deedo.Friend.Search_Somebody;
 import com.example.deedo.R;
 import com.example.deedo.area.Inquiry_Lotate;
+import com.example.deedo.background_service.GpsTracker;
 import com.example.deedo.daily.Inquiry_daily_Activity;
 import com.example.deedo.inquiry_plan.Plan;
 
@@ -27,13 +37,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Home_activity extends AppCompatActivity {
+    private GpsTracker gpsTracker;
+
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
     private TextView main_id;
     ImageButton map_button, plan_button, inquiry_button, friend_setting_button;
     String userId;
     String[] chart_d = {"first", "second", "third"};
     int [] earning = {500, 800, 1000};
     AnyChartView home_chart_view;
-
+    private Intent serviceIntent;
     /*
     액션바에 돋보기 추가
      */
@@ -66,7 +81,36 @@ public class Home_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        if (checkLocationServicesStatus()) {
+            checkRunTimePermission();
+        } else {
+            showDialogForLocationServiceSetting();
+        }
+        //이 부분을 백그라운드에서 실행하고 메인 액티비티로 넘겨줘야함
+        gpsTracker = new GpsTracker(Home_activity.this);
+        double now_latitude = gpsTracker.getLatitude();
+        double now_longitude = gpsTracker.getLongitude();
+
+
+
+    /*
+        //백그라운드 서비스 인텐트가 이전에 생성되어있는 상태에서 어플을 실행하면 기존의 intent를 종료하고 새로 서비스를 시작
+        if (BackgroundService.serviceIntent==null) {
+            serviceIntent = new Intent(this, BackgroundService.class);
+            startService(serviceIntent);
+        } else {
+            serviceIntent = BackgroundService.serviceIntent;//getInstance().getApplication();
+            Toast.makeText(getApplicationContext(), "already", Toast.LENGTH_LONG).show();
+        }
+
+
+
+
+
+        startService(new Intent(getApplicationContext(), BackgroundService.class));
+
+     */
+
         /*
         Chart View 생성
          */
@@ -85,7 +129,8 @@ public class Home_activity extends AppCompatActivity {
         inquiry_button = findViewById(R.id.inquiry_Button);
         friend_setting_button = findViewById(R.id.friend_setting_Button);
         main_id = findViewById(R.id.main_id);
-        main_id.setText(userId + "님 어서오세요!");
+        //main_id.setText(userId + "님 어서오세요!");
+        main_id.setText(now_latitude + " --" + now_longitude);
 
 
 
@@ -143,5 +188,164 @@ public class Home_activity extends AppCompatActivity {
 
         pie.data(dataEntries);
         home_chart_view.setChart(pie);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // 어플 종료 시 서비스를 중단시킨다.
+        if (serviceIntent!=null) {
+            stopService(serviceIntent);
+            serviceIntent = null;
+        }
+
+    }
+    /*
+     * ActivityCompat.requestPermissions를 사용한 퍼미션 요청의 결과를 리턴받는 메소드입니다.
+     */
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grandResults) {
+
+        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+
+            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
+
+            boolean check_result = true;
+
+
+            // 모든 퍼미션을 허용했는지 체크합니다.
+
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+
+
+            if (check_result) {
+
+                //위치 값을 가져올 수 있음
+                ;
+            } else {
+                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
+                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+
+                    Toast.makeText(Home_activity.this, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show();
+                    finish();
+
+
+                } else {
+
+                    Toast.makeText(Home_activity.this, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show();
+
+                }
+            }
+
+        }
+    }
+
+    void checkRunTimePermission() {
+
+        //런타임 퍼미션 처리
+        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(Home_activity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(Home_activity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+
+            // 2. 이미 퍼미션을 가지고 있다면
+            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
+
+
+            // 3.  위치 값을 가져올 수 있음
+
+
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(Home_activity.this, REQUIRED_PERMISSIONS[0])) {
+
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Toast.makeText(Home_activity.this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(Home_activity.this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+
+
+            } else {
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(Home_activity.this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+            }
+
+        }
+
+    }
+
+
+    //여기부터는 GPS 활성화를 위한 메소드들
+    private void showDialogForLocationServiceSetting() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Home_activity.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
+                + "위치 설정을 수정하실래요?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Intent callGPSSettingIntent
+                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+
+            case GPS_ENABLE_REQUEST_CODE:
+
+                //사용자가 GPS 활성 시켰는지 검사
+                if (checkLocationServicesStatus()) {
+                    if (checkLocationServicesStatus()) {
+
+                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음");
+                        checkRunTimePermission();
+                        return;
+                    }
+                }
+
+                break;
+        }
+    }
+
+    public boolean checkLocationServicesStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
 }
--- app/src/main/java/com/example/deedo/background_service/BackgroundService.java
+++ app/src/main/java/com/example/deedo/background_service/BackgroundService.java
@@ -1,31 +1,114 @@
 package com.example.deedo.background_service;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
 
+import androidx.core.app.NotificationCompat;
+
+import com.example.deedo.HOME_ETC.Home_activity;
+import com.example.deedo.R;
+
+import java.util.Calendar;
+
 public class BackgroundService extends Service {
-    public BackgroundService() {
-    }
+        private Thread mainThread;
+        public static Intent serviceIntent = null;
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-    }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
-    }
+        public BackgroundService() {
+        }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            serviceIntent = intent;
+            mainThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+                }
+            });
+            mainThread.start();
+
+            return START_NOT_STICKY;
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+
+            serviceIntent = null;
+            setAlarmTimer();
+            Thread.currentThread().interrupt();
+
+            if (mainThread != null) {
+                mainThread.interrupt();
+                mainThread = null;
+            }
+        }
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+        }
+
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        @Override
+        public boolean onUnbind(Intent intent) {
+            return super.onUnbind(intent);
+        }
+
+
+
+        protected void setAlarmTimer() {
+            final Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(System.currentTimeMillis());
+            c.add(Calendar.SECOND, 1);
+            Intent intent = new Intent(this, AlarmReceiver.class);
+            PendingIntent sender = PendingIntent.getBroadcast(this, 0,intent,0);
+
+            AlarmManager mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            mAlarmManager.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), sender);
+        }
+
+        private void sendNotification(String messageBody) {
+            Intent intent = new Intent(this, Home_activity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent, PendingIntent.FLAG_ONE_SHOT);
+
+            String channelId = "fcm_default_channel";//getString(R.string.default_notification_channel_id);
+            Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder =
+                    new NotificationCompat.Builder(this, channelId)
+                            .setSmallIcon(R.mipmap.ic_launcher)//drawable.splash)
+                            .setContentTitle("Service test")
+                            .setContentText(messageBody)
+                            .setAutoCancel(true)
+                            .setSound(defaultSoundUri)
+                            .setPriority(Notification.PRIORITY_HIGH)
+                            .setContentIntent(pendingIntent);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            // Since android Oreo notification channel is needed.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel(channelId,"Channel human readable title", NotificationManager.IMPORTANCE_DEFAULT);
+                notificationManager.createNotificationChannel(channel);
+            }
+
+            notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        }
     }
-}
